b9589a82dc4d4f017a9df985b3c6323e00ce795f
irina-b-dev <irinabradu.a@gmail.com>
2023-04-18 18:35:21 +0300

    added other endpoints

--- src/main/java/com/blacklabs/ctfwebapp/HelloServlet.java
+++ src/main/java/com/blacklabs/ctfwebapp/HelloServlet.java
@@ -8,9 +8,11 @@ import jakarta.servlet.annotation.*;
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
+    private String massage;
 
     public void init() {
         message = "Hello World!";
+
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -22,6 +24,8 @@ public class HelloServlet extends HttpServlet {
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
+
+
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
@@ -56,6 +60,40 @@ public class HelloServlet extends HttpServlet {
             out.println(e.getMessage());
             out.println("</body></html>");
         }
+    }
+
+    public void doOptions(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + "do options" + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void doHead(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        massage = "xxx.com/xxx"
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<a href=\""+massage+"\">" + massage + "</a>");
+        out.println("</body></html>");
+    }
+
+    public void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + "do options" + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void doTrace(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" +"Trace" + "</h1>");
+        out.println("</body></html>");
     }
 
     public void destroy() {
